0ca36ab8864358f97abaee0aac7287f5f397cdd6
Felipe Jose Llerena Catalan <llerenafelipe7@gmail.com>
2020-12-02 15:50:12 -0500

    expense tracker UI (Context API actions)

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import { v4 as uuid } from 'uuid';
+
+import {GlobalContext} from './../context/GlobalState'
 
 const AddTransaction = () => {
+  const {addTransaction} = useContext(GlobalContext)
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const newTransaction = { id: uuid(), text, amount: +amount }
+    addTransaction(newTransaction)
+    setText('')
+    setAmount(0)
+  }
 
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form onSubmit={handleSubmit} id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,22 +2,34 @@ import React, {createContext, useReducer} from 'react';
 import AppReducer from './AppReducer'
 
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ]
+  transactions: []
 };
 
 export const GlobalContext = createContext(initialState)
 
 export const GlobalProvider = props => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  const deleteTransaction = id => {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    })
+  }
+
+  const addTransaction = transaction => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
+
   return (
     <GlobalContext.Provider
       value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        addTransaction,
+        deleteTransaction
       }}
     >
       {props.children}
